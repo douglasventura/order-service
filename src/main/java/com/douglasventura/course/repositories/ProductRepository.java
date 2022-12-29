a79ed1fe3b7d8c818caff2e4a492ca450e0a6fb6
Douglas Ventura <douglasventura.pe@gmail.com>
2022-12-29 00:02:27 -0300

    Product added

---
+++ src/main/java/com/douglasventura/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.douglasventura.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglasventura.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/douglasventura/course/resources/ProductResource.java
@@ -0,0 +1,34 @@
+package com.douglasventura.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.douglasventura.course.entities.Product;
+import com.douglasventura.course.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+    @Autowired
+    private ProductService userService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Product user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+    
+}
