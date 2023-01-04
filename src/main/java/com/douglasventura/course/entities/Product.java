1f5b969d85a9a0bcb082b7ae0ff42c04f0282cae
Douglas Ventura <douglasventura.pe@gmail.com>
2023-01-04 20:09:38 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/douglasventura/course/entities/Product.java
+++ src/main/java/com/douglasventura/course/entities/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,6 +32,9 @@ public class Product implements Serializable {
   @ManyToMany
   @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
   private Set<Category> categories = new HashSet<>();
+
+  @OneToMany(mappedBy = "id.product")
+  private Set<OrderItem> orders = new HashSet<>();
 
   // empty constructor
   public Product() {
@@ -84,6 +90,17 @@ public class Product implements Serializable {
 
   public Set<Category> getCategories() {
     return categories;
+  }
+
+  @JsonIgnore
+  public Set<Order> getOrders() {
+    Set<Order> auxList = new HashSet<>();
+
+    for (OrderItem orderItem : orders) {
+      auxList.add(orderItem.getOrder());
+    }
+
+    return auxList;
   }
 
   @Override
