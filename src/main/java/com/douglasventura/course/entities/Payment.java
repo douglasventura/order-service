5452dd5d97a574724d1a68d75ec419ec7ca78b8e
Douglas Ventura <douglasventura.pe@gmail.com>
2023-01-05 17:40:43 -0300

    Subtotal & Total methods

--- src/main/java/com/douglasventura/course/entities/Payment.java
+++ src/main/java/com/douglasventura/course/entities/Payment.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +27,7 @@ public class Payment implements Serializable {
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
   private Instant moment;
 
+  @JsonIgnore
   @OneToOne
   @MapsId
   private Order order;
