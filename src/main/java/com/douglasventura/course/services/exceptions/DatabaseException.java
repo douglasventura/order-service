aaee01942d774f6a956b4b3809edf857700ab6cc
Douglas Ventura <douglasventura.pe@gmail.com>
2023-01-09 18:03:34 -0300

    Update DatabaseException

--- src/main/java/com/douglasventura/course/services/exceptions/DatabaseException.java
+++ src/main/java/com/douglasventura/course/services/exceptions/DatabaseException.java
@@ -1,8 +1,5 @@
 package com.douglasventura.course.services.exceptions;
 
-/**
- * DatabaseException
- */
 public class DatabaseException extends RuntimeException {
 
   private static final long serialVersionUID = 1L;
